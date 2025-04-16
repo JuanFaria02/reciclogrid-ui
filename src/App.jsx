fa96fdd0365af181285ec13301489b8b751a9fcc
juan <juan.faria@tecsinapse.com.br>
2025-04-15 23:03:24 -0300

    Configurado eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <div>
-       <h1 class="text-3xl font-bold underline">
-         Iniciando Projeto Reciclogrid
-      </h1>
+      <div>
+        <h1 class="text-3xl font-bold underline">
+          Iniciando Projeto Reciclogrid
+        </h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
